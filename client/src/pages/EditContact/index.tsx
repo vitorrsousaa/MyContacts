44225186dd8346e7a3e80697290299c3cd775f12
Vitor Sousa <v.sousa.cf@gmail.com>
2022-11-29 08:43:58 -0300

    create component input and select

--- client/src/pages/EditContact/index.tsx
+++ client/src/pages/EditContact/index.tsx
@@ -3,7 +3,6 @@ import PageHeader from '../../components/PageHeader';
 const EditContact = () => {
   return (
     <>
-      <h1>EditContact</h1>
       <PageHeader title="Editar Mateus Silva" />
     </>
   );
--- client/src/pages/NewContact/index.tsx
+++ client/src/pages/NewContact/index.tsx
@@ -3,7 +3,6 @@ import PageHeader from '../../components/PageHeader';
 const NewContact = () => {
   return (
     <>
-      <h1>New Contact</h1>
       <PageHeader title="Novo Contato" />
     </>
   );
