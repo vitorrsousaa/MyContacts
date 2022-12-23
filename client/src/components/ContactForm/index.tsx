2a83c6aa7a3da1eefe8f8ee86fc1705a943547ef
Vitor Sousa <v.sousa.cf@gmail.com>
2022-12-23 15:00:55 -0300

    Input: validating data

--- client/src/components/ContactForm/index.tsx
+++ client/src/components/ContactForm/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { BaseSyntheticEvent, useState } from 'react';
+import isEmailValid from '../../utils/isEmailValid';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import { Form, ButtonContainer } from './styles';
+
+interface ErrorsProps {
+  field: string;
+  message: string;
+}
 
 interface ContactFormProps {
   buttonLabel: string;
@@ -14,28 +20,61 @@ const ContactForm = ({ buttonLabel }: ContactFormProps) => {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState<ErrorsProps[]>([]);
 
-  const handleSubmit = (event: React.SyntheticEvent) => {
+  function handleNameChange(event: BaseSyntheticEvent) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [...prevState, { field: 'name', message: 'Nome é obrigatório.' }]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  }
+
+  function handleEmailChange(event: BaseSyntheticEvent) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [...prevState, { field: 'email', message: 'E-mail inválido' }]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName: string) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  function handleSubmit(event: React.SyntheticEvent) {
     console.log(event);
     event.preventDefault();
-  };
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
           placeholder="Nome"
           type="text"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
+          error={!!getErrorMessageByFieldName('name')}
         />
       </FormGroup>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
           placeholder="E-mail"
           type="email"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
+          error={!!getErrorMessageByFieldName('email')}
         />
       </FormGroup>
       <FormGroup>
