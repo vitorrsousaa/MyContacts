b1890f63b1ac156ccc5f6db571fb38a6466bf7e4
Vitor Sousa <v.sousa.cf@gmail.com>
2024-02-06 15:51:23 -0300

    includes cors origins

--- api/src/app/middlewares/cors.ts
+++ api/src/app/middlewares/cors.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
 export default function cors(request: Request, response: Response, next: NextFunction) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+
+  const origin = request.header('origin');
+
+  const isAllowed = allowedOrigins.includes(origin);
+
+  response.setHeader('Access-Control-Allow-Origin', origin || '*');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '20');
